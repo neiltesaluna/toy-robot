532c1e10f70a75448005d8c274cfd57a296c0143
neiltesaluna <neil.tesaluna@gmail.com>
2020-01-09 10:58:04 +1100

    Added an exit command and validation

--- toy_robot.py
+++ toy_robot.py
@@ -1,7 +1,11 @@
+from sys import exit
+
+
 def initial_robot(x_robot, y_robot, current_facing):
 
     if (x_robot > max_wtable or x_robot < 0) or (y_robot > max_ltable or y_robot < 0):
         print("\nYour robot is off the table!")
+        print("For a table of {}W x {}L".format(max_wtable,max_ltable))
         print("Enter your robots position again")
         return reposition(x_robot,y_robot,current_facing)
 
@@ -14,6 +18,7 @@ def initial_robot(x_robot, y_robot, current_facing):
 
 
 
+
 def command(x_robot, y_robot, current_facing):
 
     print("What commands did you want to give the robot?")
@@ -22,11 +27,12 @@ def command(x_robot, y_robot, current_facing):
         ROTATE
         REPORT
         REPOSITION
+        END
     """)
     user_cmd = input()
 
     if user_cmd == 'ROTATE':
-        return robot_rotate(current_facing)
+        return robot_rotate(x_robot, y_robot, current_facing)
 
     elif user_cmd == 'REPORT':
         return robot_position(x_robot, y_robot, current_facing)
@@ -34,13 +40,18 @@ def command(x_robot, y_robot, current_facing):
     elif user_cmd == 'MOVE':
         return moving_robot(x_robot, y_robot, current_facing)
 
-    elif user_ == 'REPOSITION':
+    elif user_cmd == 'REPOSITION':
         return reposition(x_robot, y_robot, current_facing)
 
-    else:
-        print("That's not a vaild input!")
-        return robot_position(x_robot, y_robot, current_facing)
+    elif user_cmd == 'END':
+        print("For a table of {}W x {}L".format(max_wtable,max_ltable))
+        print("Your robot is currently at {}, {} facing {}.".format(x_robot,y_robot,current_facing))
+        print("Thanks for playing!")
+        exit(0)
 
+    else:
+        print("That can't be right...")
+        return command(x_robot, y_robot, current_facing)
 
 
 
@@ -67,6 +78,8 @@ def robot_position(x_robot, y_robot, current_facing):
     return command(x_robot, y_robot, current_facing)
 
 
+
+
 def reposition(x_robot, y_robot, current_facing):
     x_robot = int(input("x axis = "))
     y_robot = int(input("y axis = "))
@@ -76,36 +89,52 @@ def reposition(x_robot, y_robot, current_facing):
 
 
 
-def robot_rotate(current_facing):
-    rotate_cmd = input("""
+
+def robot_rotate(x_robot, y_robot, current_facing):
+    user_cmd = input("""
 which way did you want to turn?
         RIGHT
         LEFT
 """)
 
-    if rotate_cmd == 'RIGHT':
-        if current_facing == 'NORTH':
-            current_facing = 'EAST'
-        elif current_facing == 'EAST':
-            current_facing = 'SOUTH'
-        elif current_facing == 'SOUTH':
-            current_facing = 'WEST'
-        elif current_facing == 'WEST':
-            current_facing = 'NORTH'
-
-    if rotate_cmd == 'LEFT':
-        if current_facing == 'NORTH':
-            current_facing = 'WEST'
-        elif current_facing == 'EAST':
-            current_facing = 'NORTH'
-        elif current_facing == 'SOUTH':
-            current_facing = 'EAST'
-        elif current_facing == 'WEST':
-            current_facing = 'SOUTH'
+    if user_cmd == 'LEFT':
+        return rotate_left(x_robot, y_robot, current_facing)
+    if user_cmd == 'RIGHT':
+        return rotate_right(x_robot, y_robot, current_facing)
 
     else:
         print("That's not a vaild input!")
+        return robot_rotate(x_robot, y_robot, current_facing)
 
+
+
+
+def rotate_right(x_robot, y_robot, current_facing):
+
+    if current_facing == 'NORTH':
+            current_facing = 'EAST'
+    elif current_facing == 'EAST':
+            current_facing = 'SOUTH'
+    elif current_facing == 'SOUTH':
+            current_facing = 'WEST'
+    elif current_facing == 'WEST':
+            current_facing = 'NORTH'
+
+    return robot_position(x_robot, y_robot, current_facing)
+
+
+
+
+def rotate_left(x_robot, y_robot, current_facing):
+
+    if current_facing == 'NORTH':
+            current_facing = 'WEST'
+    elif current_facing == 'EAST':
+            current_facing = 'NORTH'
+    elif current_facing == 'SOUTH':
+            current_facing = 'EAST'
+    elif current_facing == 'WEST':
+            current_facing = 'SOUTH'
     return robot_position(x_robot, y_robot, current_facing)
 
 
@@ -118,12 +147,12 @@ which way did you want to move?
         BACK
 """)
     if user_cmd == 'FORWARD':
-        move_back(x_robot, y_robot, current_facing)
+        return move_forward(x_robot, y_robot, current_facing)
     if user_cmd == 'BACK':
-        move_forward(x_robot, y_robot, current_facing)
+        return move_back(x_robot, y_robot, current_facing)
     else:
-        print("That's not a vaild input!")
-        robot_position(x_robot, y_robot, current_facing)
+        print("That can't be right...")
+        return moving_robot(x_robot, y_robot, current_facing)
 
 
 
@@ -143,6 +172,8 @@ def move_back(x_robot, y_robot, current_facing):
         print("That can't be right...")
 
     return robot_position(x_robot, y_robot, current_facing)
+
+
 
 
 def move_forward(x_robot, y_robot, current_facing):
@@ -173,10 +204,4 @@ max_ltable = int(input("What is the length of this table? "))
 print("For a table of {}W x {}L".format(max_wtable,max_ltable))
 print("\nPlease enter where you want to put the robot")
 
-x_robot = 0
-y_robot = 0
-current_facing = 'NORTH'
-
-reposition(x_robot, y_robot, current_facing)
-
-initial_robot(x_robot,y_robot, current_facing)
+reposition(0,0,'NORTH')
